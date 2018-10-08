0801827cfd14d437d01bf97a9ba7213c67de9bc1
Maham Shahid <mahamshahid18@users.noreply.github.com>
2018-10-08 17:40:19 +0500

    Issue#28: Redesigned project page (#32)

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,10 +1,116 @@
 import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
+
+import { withStyles } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+
 import AppService from '../../services/AppService';
 
-export default class Project extends Component {
+const styles = theme => ({
+  root: {
+  },
+  card: {
+    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+    margin: '0px auto',
+    marginTop: '100px',
+    textAlign: 'left',
+    transition: '0.3s',
+    padding: '10px',
+    '&:hover': {
+      boxShadow: ' 0 8px 16px 0 rgba(0,0,0,0.2)',
+    }
+  },
+  title: {
+    fontSize: '1.75rem !important',
+    letterSpacing: '0.25px !important',
+    marginBottom: '20px !important',
+    textAlign: 'center !important',
+    color: '#9f5afd!important'
+  },
+  description: {
+    marginTop: '25px !important',
+    marginBottom: '35px !important',
+  },
+  linksContainer: {
+    margin: '20px 0px !important',
+    textAlign: 'center',
+  },
+  linkItem: {
+    margin: '0px 5px',
+    textDecoration: 'none',
+    padding: '2px 12px',
+    borderRadius: '50px',
+    border: '1px solid gray',
+    color: '#512da8',
+    '&:hover': {
+      color: '#3700b7',
+    },
+  },
+  Website: {
+    border: '2px solid #ff9478',
+    '&:hover': {
+      border: '3px solid #ff9478',
+    }
+  },
+  Repo: {
+    border: '2px solid #f64747',
+    '&:hover': {
+      border: '3px solid #f64747',
+    }
+  },
+  Chat: {
+    border: '2px solid #f03434',
+    '&:hover': {
+      border: '3px solid #f03434',
+    }
+  },
+  chipDivider: {
+    margin: '10px 0px !important',
+  },
+  techContainer: {
+    margin: '20px 0px !important',
+  },
+  resourcesContainer: {
+    margin: '20px 0px !important',
+  },
+  techTitle: {
+    color: '#231e1e !important',
+    fontWeight: '500 !important',
+    fontSize: '0.975rem !important',
+    textTransform: 'uppercase',
+    wordSpacing: '1px',
+    letterSpacing: '0.25px',
+  },
+  resourcesTitle: {
+    color: '#231e1e !important',
+    fontWeight: '500 !important',
+    fontSize: '0.975rem !important',
+    textTransform: 'uppercase',
+    wordSpacing: '1px',
+    letterSpacing: '0.25px',
+  }
+});
+
+
+const getLinkElement = (linkName, linkValue, classObj) => {
+  if (linkValue !== '') {
+    return (
+      <a
+        className={`${classObj.linkItem} ${classObj[linkName]}`}
+        href={linkValue}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {linkName}
+      </a>
+    );
+  }
+}
+
+class Project extends Component {
   constructor(props) {
     super(props);
     this.state = { project: null };
@@ -23,74 +129,52 @@ export default class Project extends Component {
 
   render() {
     const { project } = this.state;
+    const { classes } = this.props;
 
     return (
-      <Grid container justify="center" spacing={0}>
-        <Grid item xs={12}>
-          <br />
-          <Typography
-            styles={{ marginTop: '20px' }}
-            variant="display1"
-            align="center"
-            component="h1"
-            gutterBottom
-          >
-            {project ? project.name : ''}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="subheading" className="description" align="center" paragraph>
-            {project ? project.description : ''}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography className="description" align="center" paragraph>
-            <b>Tech Stack: </b>
-            {project ? project.tech : ''}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography className="description" align="center" paragraph>
-            <b>Looking for partners: </b>
-            {project ? project.partners : ''}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          {project && project.link ? (
-            <Button
-              href={project.link}
-              style={{ backgroundColor: '#ff0000b3', color: 'white', margin: '10px' }}
-              target="_blank"
-            >
-              Website
-            </Button>
-          ) : (
-            ''
-          )}
-          {project && project.chatLink ? (
-            <Button
-              href={project.chatLink}
-              style={{ backgroundColor: '#008000b3', color: 'white', margin: '10px' }}
-              target="_blank"
-            >
-              Slack/Gitter
-            </Button>
-          ) : (
-            ''
-          )}
-          {project && project.repoLink ? (
-            <Button
-              href={project.repoLink}
-              style={{ backgroundColor: '#0072ffb3', color: 'white', margin: '10px' }}
-              target="_blank"
-            >
-              Repo
-            </Button>
-          ) : (
-            ''
-          )}
+      <Grid container spacing={0}>
+        <Grid item container xs={12} spacing={0}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography className={classes.title} variant="title" component="h1">
+                {project ? project.name : ''}
+              </Typography>
+              <Typography className={classes.linksContainer} component="div">
+                {getLinkElement('Website', project && project.link ? project.link : '', classes)}
+                {getLinkElement('Repo', project && project.repoLink ? project.repoLink : '', classes)}
+                {getLinkElement('Chat', project && project.chatLink ? project.chatLink : '', classes)}
+              </Typography>
+              <Typography className={classes.description} component="p">
+                {project ? project.description : ''}
+              </Typography>
+              <Typography className={classes.techContainer} component="div">
+                <Typography className={classes.techTitle} component="h2">
+                  {'Tech Stack'}
+                </Typography>
+                <Chip
+                  label={project && project.tech ? project.tech : ''}
+                  className={classes.chipDivider}
+                  color="secondary"
+                />
+              </Typography>
+              <Typography className={classes.resourcesContainer} component="div">
+                <Typography className={classes.resourcesTitle} component="h2">
+                  {'Resources Needed'}
+                </Typography>
+                <div className={classes.resourceItems}>
+                  <Chip
+                    label={project && project.partners ? project.partners : ''}
+                    className={classes.chipDivider}
+                    color="primary"
+                  />
+                </div>
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     );
   }
 }
+
+export default withStyles(styles)(Project);
